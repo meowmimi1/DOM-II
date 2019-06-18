9b6e33b558aac4336936b0d39e7b5ee5dcd156cb
Mimi Hoang <miminhoang1@gmail.com>
2019-06-18 15:03:04 -0400

    Forked and initial push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 // Your code goes here
+// let nav = document.querySelectorAll(".nav-link");
+// nav[0].addEventListener("mouseover", (event) => {
+//     event.target.style.backgroundColor = "pink";
+// });
+
+let nav = document.querySelectorAll("nav");
+nav[0].addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = "purple";
+});
+nav[1].addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor ="green" ;
+});
+nav[2].addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = "red";
+});
+nav[3].addEventListener("mouseover", (event) => {
+ event.target.style.backgroundColor = "pink";
+})
+nav[0].addEventListener("mouseleave", (event) => {
+    event.target.style.backgroundColor = "blue ";
+  });
+  nav[1].addEventListener("mouseleave", (event) => {
+    event.target.style.backgroundColor = "blue";
+  });
+  nav[2].addEventListener("mouseleave", (event) => {
+    event.target.style.backgroundColor = "blue";
+  });
+  nav[3].addEventListener("mouseleave",(event) => {
+      event.target.style.backgroundColor = "blue";
+  })
+
+  let intro = document.querySelector(".intro h2");
+intro.addEventListener("copy", (event) => {
+  alert("Intro is copied");
+});
+
+
+const foot = document.querySelector('footer');
+ foot.addEventListener('mouseover', function(event) {
+    event.target.style.backgroundColor = 'yellow';
+})
